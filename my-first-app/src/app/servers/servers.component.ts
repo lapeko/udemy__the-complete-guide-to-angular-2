19b84f7baad816e4a79b2bb0c5d253a30ca5fe8a
lapeko <lapeko88@gmail.com>
2023-06-03 15:09:15 +0200

    use attribute selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
