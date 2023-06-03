503392bbb0cee16ffc9e8f84d880ee10180a159b
lapeko <lapeko88@gmail.com>
2023-06-03 17:21:11 +0200

    use ngStyle

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId = 10;
-  status = 'offline';
+  status: string;
+
+  constructor() {
+    this.status = Math.random() < 0.5 ? 'offline' : 'online';
+  }
+
+  get serverStatusColor(): string {
+    return this.status === 'online' ? 'green' : 'red';
+  }
 }
