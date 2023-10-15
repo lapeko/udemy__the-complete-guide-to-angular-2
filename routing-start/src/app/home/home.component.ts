009c9d5a70f8baf246cc52a1145d20b3d87f5aef
lapeko <lapeko88@gmail.com>
2023-10-15 17:38:51 +0200

    refactor with nested routes

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,24 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-  }
-
-  visitServers() {
-    this.router.navigate(["servers"]);
-  }
-
-  visitServer(id: number) {
-    this.router
-      .navigate(["servers", id, "edit"], {queryParams: {allowEdit: true}, fragment: "some-fragment"});
-  }
+  constructor() { }
 }
