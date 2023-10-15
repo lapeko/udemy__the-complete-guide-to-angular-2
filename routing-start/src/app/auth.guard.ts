d52398ba965a50aefd134ff0ead7f8b983bb981d
lapeko <lapeko88@gmail.com>
2023-10-15 20:41:18 +0200

    use canActivateChild

--- routing-start/src/app/auth.guard.ts
+++ routing-start/src/app/auth.guard.ts
@@ -1,23 +1,38 @@
 import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from "@angular/router";
 import {Observable} from "rxjs";
+
 import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(
     private router: Router,
     private authService: AuthService,
   ) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.isAuthenticated().then(loggedIn => {
-      if (loggedIn) return true;
-      else this.router.navigate(['/']);
-    });
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isAuthenticated()
+      .then(loggedIn => loggedIn ? true : this.router.parseUrl('/'));
   }
 
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivate(childRoute, state);
+  }
 }
