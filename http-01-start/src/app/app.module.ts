21ef8aa1fecf46743b6c3fc7aad1e0aa00fb53a7
lapeko <lapeko88@gmail.com>
2023-10-21 12:52:13 +0200

    Create simple interceptor

--- http-01-start/src/app/app.module.ts
+++ http-01-start/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {AuthInterceptor} from "./auth.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
