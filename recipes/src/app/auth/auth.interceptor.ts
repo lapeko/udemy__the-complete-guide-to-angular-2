639d442cfea9c58d83255adca0d5d842dd737fbb
lapeko <lapeko88@gmail.com>
2023-10-21 21:58:26 +0200

    simplify interceptor

--- recipes/src/app/auth/auth.interceptor.ts
+++ recipes/src/app/auth/auth.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
 
 @Injectable()
@@ -18,7 +18,6 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.user$.value?.token;
     if (!token) return next.handle(request);
-    const modifiedRequest = request.clone({params: request.params.append("auth", token)});
-    return next.handle(modifiedRequest);
+    return next.handle(request.clone({params: request.params.append("auth", token)}));
   }
 }
