b7d664fed368242ea21046eada20c0de84516320
lapeko <lapeko88@gmail.com>
2023-10-22 14:56:50 +0200

    preload all modules

--- recipes/src/app/app-routing.module.ts
+++ recipes/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from "@angular/core";
-import {Route, RouterModule} from "@angular/router";
+import {PreloadAllModules, Route, RouterModule} from "@angular/router";
 
 import {RecipesComponent} from "./recipes/recipes.component";
 import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
@@ -21,7 +21,7 @@ const routes: Route[] = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
