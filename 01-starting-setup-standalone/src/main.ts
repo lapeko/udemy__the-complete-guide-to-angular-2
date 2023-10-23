ed3bd4c6cc9fe1ab462675799fbb967542ba0456
lapeko <lapeko88@gmail.com>
2023-10-23 17:11:42 +0200

    delete ngrx standalone project

--- 01-starting-setup-standalone/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-import { provideStore } from '@ngrx/store';
-import {counterReducer} from "./app/strore/counter.reducer";
-
-bootstrapApplication(AppComponent, {
-    providers: [provideStore({counter: counterReducer})]
-});
