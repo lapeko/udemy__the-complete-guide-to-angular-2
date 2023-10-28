624cafa71b99f8efbc93c9013f1029269a27d222
lapeko <lapeko88@gmail.com>
2023-10-28 17:00:05 +0200

    small refactoring

--- recipes/src/main.ts
+++ recipes/src/main.ts
@@ -3,13 +3,13 @@ import {provideStore} from '@ngrx/store';
 import {provideRouter} from "@angular/router";
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {provideEffects} from "@ngrx/effects";
+import {isDevMode} from "@angular/core";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
 
 import {AppComponent} from "./app/app.component";
 import {APP_ROUTES} from "./app/app.routes";
 import {AuthInterceptor} from "./app/auth/auth.interceptor";
 import {reducers, effects} from "./store";
-import {isDevMode} from "@angular/core";
-import {provideStoreDevtools} from "@ngrx/store-devtools";
 
 isDevMode()
 
