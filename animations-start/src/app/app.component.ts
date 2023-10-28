66aa4efc9d5b2bea12a7678271c59c753a1429e2
lapeko <lapeko88@gmail.com>
2023-10-28 21:39:51 +0200

    init animation project

---
+++ animations-start/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item: string) {
+      this.list.push(item);
+    }
+
+  onDelete(item: string) {
+    console.log('Not implemented');
+  }
+}
